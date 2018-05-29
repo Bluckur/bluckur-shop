a13e180b73e540f482754537c2d39cfb3b408efa
Pim Janissen <p.janissen@student.fontys.nl>
2018-05-29 15:28:32 +0200

    Authentication method
    User can use the authentication method with the hardcoded password.
    Returns the jwt token.

--- Shop Core/src/main/java/application/Application.java
+++ Shop Core/src/main/java/application/Application.java
@@ -12,9 +12,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import security.JwtFilter;
 import service.CustomerService;
 import service.ProductService;
 import service.PurchaseService;
@@ -40,6 +43,15 @@ public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean jwtFilter() {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new JwtFilter());
+        registrationBean.addUrlPatterns("/product/*");
+
+        return registrationBean;
     }
 
     @Override
--- Shop Core/src/main/java/rest/AuthenticationRest.java
+++ Shop Core/src/main/java/rest/AuthenticationRest.java
@@ -1,11 +1,16 @@
 package rest;
 
 import domain.Customer;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import service.AuthenticationService;
+
+import javax.servlet.ServletException;
+import java.util.Date;
 
 @RestController
 public class AuthenticationRest {
@@ -13,8 +18,23 @@ public class AuthenticationRest {
     @Autowired
     private AuthenticationService authenticationService;
 
-    @RequestMapping("/authentication/greeting")
-    public Customer customer(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Customer(name, "Rachelsmolen R1 Eindhoven\np.janissen@student.fontys.nl");
+    @RequestMapping("/authentication/login")
+    public String login(@RequestParam("identifier") String identifier, @RequestParam("password") String password) throws ServletException {
+        final String adminPassword="QWERTY12345";
+
+        String jwtToken = "";
+
+        if (password == null || password.isEmpty()) {
+            throw new ServletException("Please fill in username and password");
+        }
+
+        if (!password.equals(adminPassword)) {
+            throw new ServletException("Invalid login. Please check your name and password.");
+        }
+
+        jwtToken = Jwts.builder().setSubject(identifier).claim("roles", "ADMIN").setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, "SecretKey").compact();
+
+        return jwtToken;
     }
 }
